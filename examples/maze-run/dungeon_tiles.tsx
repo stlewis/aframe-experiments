a5fe89dc19fa1ae0afd4868b3b1a8d4b2b1ef078
Steve Lewis <steve@decodingsteve.com>
2018-03-29 17:22:55 -0700

    Even more cleanup
    Also get basic kinematic bodies working again

--- examples/maze-run/dungeon_tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="dungeon_tiles" tilewidth="32" tileheight="32" tilecount="132" columns="11">
- <image source="../../../../../../../Downloads/dungeon_tiles.png" width="368" height="384"/>
-</tileset>
