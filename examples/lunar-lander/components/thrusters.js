918dd93f19e2d17fa2ef433a16fcdbd92a7ae6ef
Steve Lewis <steve@decodingsteve.com>
2018-03-30 13:19:43 -0700

    Very simple HUD

--- examples/lunar-lander/components/thrusters.js
+++ examples/lunar-lander/components/thrusters.js
@@ -1,23 +1,25 @@
 AFRAME.registerComponent('thrusters', {
   schema: {
-    fuelLevel: {default: 500 }
+    fuelLevel: {default: 500 },
+    thrusterVelocity: {default: 5 }
   },
 
   init: function() {
     this.keyboardControls = this.el.components['keyboard-controls'];
     this.movementControls = this.el.components['movement-controls'];
-    this.body = this.el.components['kinematic-body'].body;
+    this.scoreHUD = document.querySelector('#score-hud');
+    this.fuelHUD = document.querySelector('#fuel-hud');
+
+    this.scoreHUD.setAttribute('text', { value: 'Score: 0', align: 'right'});
+    this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
   },
 
   tick: function() {
-    fuelHUD  = document.querySelector("#fuel-hud");
 
     if(this.keyboardControls.isPressed('Space') && this.data.fuelLevel > 0){
        this.data.fuelLevel = this.data.fuelLevel - 1;
-      fuelHUD.setAttribute('text', "value: " + this.data.fuelLevel);
-
-
-      this.movementControls.velocity.y = 5
+       this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
+       this.movementControls.velocity.y = this.data.thrusterVelocity;
     }
 
   }
