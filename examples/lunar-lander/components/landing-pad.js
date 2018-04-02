e77cce6f38d702738b5456ec38810a2e7a689c13
Steve Lewis <steve@decodingsteve.com>
2018-04-02 08:51:40 -0700

    Smooth out vertical movement mechanics

---
+++ examples/lunar-lander/components/landing-pad.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent('landing-pad', {
+
+  init: function() {
+    this.el.addEventListener('collide', function(e){
+    // It's a "good" contact if the Y value of this vector is negative...
+    });
+  }
+
+});
--- examples/lunar-lander/components/thrusters.js
+++ examples/lunar-lander/components/thrusters.js
@@ -1,27 +1,42 @@
 AFRAME.registerComponent('thrusters', {
   schema: {
     fuelLevel: {default: 500 },
-    thrusterVelocity: {default: 5 }
+    thrusterVelocityMax: {default: 5 }
   },
 
   init: function() {
     this.keyboardControls = this.el.components['keyboard-controls'];
     this.movementControls = this.el.components['movement-controls'];
+    this.body             = this.el.components['kinematic-body'].body;
     this.scoreHUD = document.querySelector('#score-hud');
     this.fuelHUD = document.querySelector('#fuel-hud');
+    // NOTE speedHUD to display downward velocity.
+    // We also need to decide what the "safe" landing range in in terms of speed.
 
     this.scoreHUD.setAttribute('text', { value: 'Score: 0', align: 'right'});
     this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
+
+    this.thrusterEngagedSecs = 0;
   },
 
-  tick: function() {
+  tick: function(globalUp, timeDelta) {
+    console.log("Controls: " + this.movementControls.velocity.y, "Body: " + this.body.velocity.y);
 
     if(this.keyboardControls.isPressed('Space') && this.data.fuelLevel > 0){
-       this.data.fuelLevel = this.data.fuelLevel - 1;
-       this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
-       this.movementControls.velocity.y = this.data.thrusterVelocity;
-    }
 
+      this.data.fuelLevel = this.data.fuelLevel - 1;
+      this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
+
+      if(this.movementControls.velocity.y < this.data.thrusterVelocityMax){
+        this.thrusterEngagedSecs = this.thrusterEngagedSecs + (timeDelta/1000)
+        this.movementControls.velocity.y = (0.5 * this.thrusterEngagedSecs);
+      }else{
+        this.movementControls.velocity.y = this.data.thrusterVelocityMax;
+      }
+
+    }else{
+      this.movementControls.velocity.y = 0;
+    }
   }
 
 });
