404019f535182240c7371180fd673b0a64455038
Steve Lewis <steve@decodingsteve.com>
2018-04-02 16:32:00 -0700

    Move collision detection to platforms
    This allows us to easily update platform state upon collision, ensuring
    that a landing only counts once per platform.

--- examples/lunar-lander/components/thrusters.js
+++ examples/lunar-lander/components/thrusters.js
@@ -2,43 +2,22 @@ AFRAME.registerComponent('thrusters', {
   schema: {
     fuelLevel: {default: 500 },
     thrusterVelocityMax: {default: 5 },
-    score: {default: 0 }
   },
 
   init: function() {
-
-    this.score = 0;
-
-    this.onCollision = this.onCollision.bind(this);
-
-    this.el.addEventListener('collide', this.onCollision)
-
     this.keyboardControls = this.el.components['keyboard-controls'];
     this.movementControls = this.el.components['movement-controls'];
     this.body             = this.el.components['kinematic-body'].body;
-    this.scoreHUD = document.querySelector('#score-hud');
     this.fuelHUD = document.querySelector('#fuel-hud');
     this.speedHUD = document.querySelector("#speed-hud")
     // We also need to decide what the "safe" landing range in in terms of speed.
-
-    this.scoreHUD.setAttribute('text', { value: 'Score: 0', anchor: 'right'});
     this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
     this.speedHUD.setAttribute('text', { value: "Vertical Speed: 0", anchor: 'right', baseline: 'bottom'});
 
     this.thrusterEngagedSecs = 0;
   },
 
-  onCollision: function(e) {
-    var contactNormal = e.detail.contact.ni
-    var verticalContact = contactNormal.y
-    if(verticalContact < 0 ) {
-      this.data.score += 10
-      console.log("Score!");
-    }
-  },
-
   tick: function(globalUp, timeDelta) {
-    this.scoreHUD.setAttribute('text', {value: "Score: " + this.data.score })
     this.speedHUD.setAttribute('text', {value: "Vertical Speed: " + Math.round(this.body.velocity.y) })
     if(this.keyboardControls.isPressed('Space') && this.data.fuelLevel > 0){
 
