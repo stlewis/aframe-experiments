31573ddc7729c1153843b013ca1f2cf2be98fc6b
Steve Lewis <steve@decodingsteve.com>
2018-04-02 16:11:15 -0700

    A very basic idea of scoring
    This was mostly just so that we could register collision events. Rather
    than registering it on the platforms, registering it on the Thruster.
    This is probably not the way to do this, since the platform itself will
    know if it's been 'hit' or not already, and will update the score
    accordingly.
    
    That of course leads to the realization that the score might need to
    exist inside a separate, more "global" entity.

--- examples/lunar-lander/components/thrusters.js
+++ examples/lunar-lander/components/thrusters.js
@@ -1,27 +1,45 @@
 AFRAME.registerComponent('thrusters', {
   schema: {
     fuelLevel: {default: 500 },
-    thrusterVelocityMax: {default: 5 }
+    thrusterVelocityMax: {default: 5 },
+    score: {default: 0 }
   },
 
   init: function() {
+
+    this.score = 0;
+
+    this.onCollision = this.onCollision.bind(this);
+
+    this.el.addEventListener('collide', this.onCollision)
+
     this.keyboardControls = this.el.components['keyboard-controls'];
     this.movementControls = this.el.components['movement-controls'];
     this.body             = this.el.components['kinematic-body'].body;
     this.scoreHUD = document.querySelector('#score-hud');
     this.fuelHUD = document.querySelector('#fuel-hud');
-    // NOTE speedHUD to display downward velocity.
+    this.speedHUD = document.querySelector("#speed-hud")
     // We also need to decide what the "safe" landing range in in terms of speed.
 
-    this.scoreHUD.setAttribute('text', { value: 'Score: 0', align: 'right'});
+    this.scoreHUD.setAttribute('text', { value: 'Score: 0', anchor: 'right'});
     this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
+    this.speedHUD.setAttribute('text', { value: "Vertical Speed: 0", anchor: 'right', baseline: 'bottom'});
 
     this.thrusterEngagedSecs = 0;
   },
 
-  tick: function(globalUp, timeDelta) {
-    console.log("Controls: " + this.movementControls.velocity.y, "Body: " + this.body.velocity.y);
+  onCollision: function(e) {
+    var contactNormal = e.detail.contact.ni
+    var verticalContact = contactNormal.y
+    if(verticalContact < 0 ) {
+      this.data.score += 10
+      console.log("Score!");
+    }
+  },
 
+  tick: function(globalUp, timeDelta) {
+    this.scoreHUD.setAttribute('text', {value: "Score: " + this.data.score })
+    this.speedHUD.setAttribute('text', {value: "Vertical Speed: " + Math.round(this.body.velocity.y) })
     if(this.keyboardControls.isPressed('Space') && this.data.fuelLevel > 0){
 
       this.data.fuelLevel = this.data.fuelLevel - 1;
@@ -29,13 +47,14 @@ AFRAME.registerComponent('thrusters', {
 
       if(this.movementControls.velocity.y < this.data.thrusterVelocityMax){
         this.thrusterEngagedSecs = this.thrusterEngagedSecs + (timeDelta/1000)
-        this.movementControls.velocity.y = (0.5 * this.thrusterEngagedSecs);
+        this.movementControls.velocity.y = (2 * this.thrusterEngagedSecs);
       }else{
-        this.movementControls.velocity.y = this.data.thrusterVelocityMax;
+        this.movementControls.y = this.data.thrusterVelocityMax;
       }
 
     }else{
-      this.movementControls.velocity.y = 0;
+      this.thrusterEngagedSecs = 0;
+      //this.movementControls.velocity.y = 0;
     }
   }
 
