4b8b1547034bcf6989ae4efb4c2188580b009b5a
Steve Lewis <steve@decodingsteve.com>
2018-04-03 15:42:30 -0700

    Show altitude in HUD and increase thruster speed

--- examples/lunar-lander/components/thrusters.js
+++ examples/lunar-lander/components/thrusters.js
@@ -10,15 +10,18 @@ AFRAME.registerComponent('thrusters', {
     this.body             = this.el.components['kinematic-body'].body;
     this.fuelHUD = document.querySelector('#fuel-hud');
     this.speedHUD = document.querySelector("#speed-hud")
+    this.altitudeHUD = document.querySelector("#altitude-hud")
     // We also need to decide what the "safe" landing range in in terms of speed.
     this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
     this.speedHUD.setAttribute('text', { value: "Vertical Speed: 0", anchor: 'right', baseline: 'bottom'});
+    this.altitudeHUD.setAttribute('text', { value: "AGL: 0", anchor: 'right', baseline: 'top'});
 
     this.thrusterEngagedSecs = 0;
   },
 
   tick: function(globalUp, timeDelta) {
     this.speedHUD.setAttribute('text', {value: "Vertical Speed: " + Math.round(this.body.velocity.y) })
+    this.altitudeHUD.setAttribute('text', {value: "AGL: " + Math.round(this.body.position.y) })
     if(this.keyboardControls.isPressed('Space') && this.data.fuelLevel > 0){
 
       this.data.fuelLevel = this.data.fuelLevel - 1;
@@ -26,7 +29,7 @@ AFRAME.registerComponent('thrusters', {
 
       if(this.movementControls.velocity.y < this.data.thrusterVelocityMax){
         this.thrusterEngagedSecs = this.thrusterEngagedSecs + (timeDelta/1000)
-        this.movementControls.velocity.y = (2 * this.thrusterEngagedSecs);
+        this.movementControls.velocity.y = (8 * this.thrusterEngagedSecs);
       }else{
         this.movementControls.y = this.data.thrusterVelocityMax;
       }
