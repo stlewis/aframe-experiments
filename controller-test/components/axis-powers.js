ccf400896cce8941bbb1af5692e340b93619e22b
Steve Lewis <steve@decodingsteve.com>
2019-04-04 09:10:28 -0700

    Set up experiment parameters

--- controller-test/components/axis-powers.js
+++ controller-test/components/axis-powers.js
@@ -1,13 +1,14 @@
 AFRAME.registerComponent('axis-powers', {
 
   init: function() {
+    AFRAME.log("Place your thumb on a position on the trackpad.");
+    AFRAME.log("Then press the trigger to log your thumb position");
     this.el.addEventListener('triggerdown', this.triggerDown.bind(this));
   },
 
   triggerDown: function() {
-    AFRAME.log("M: " + window.navigator.getGamepads()[0].mapping);
-    //AFRAME.log("0: " + window.navigator.getGamepads()[0].axes[0]);
-    //AFRAME.log("1: " + window.navigator.getGamepads()[0].axes[1]);
+    AFRAME.log("X Axis: " + window.navigator.getGamepads()[0].axes[0]);
+    AFRAME.log("Y Axis: " + window.navigator.getGamepads()[0].axes[1]);
   },
 
 });
