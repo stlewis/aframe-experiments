1e143af5c63835b73c161b107e74abb35b9511c1
Steve Lewis <steve.l@lojistic.com>
2019-04-12 16:29:48 -0700

    Controller test stuff

--- controller-test/components/axis-powers.js
+++ controller-test/components/axis-powers.js
@@ -3,12 +3,12 @@ AFRAME.registerComponent('axis-powers', {
   init: function() {
     AFRAME.log("Place your thumb on a position on the trackpad.");
     AFRAME.log("Then press the trigger to log your thumb position");
-    this.el.addEventListener('touchstart', this.triggerDown.bind(this));
+    this.el.addEventListener('trackpadchanged', this.triggerDown.bind(this));
   },
 
   triggerDown: function() {
-    AFRAME.log("X Axis: " + window.navigator.getGamepads()[1].axes[0]);
-    AFRAME.log("Y Axis: " + window.navigator.getGamepads()[1].axes[1]);
+    AFRAME.log("X Axis: " + window.navigator.getGamepads()[0].axes[0]);
+    AFRAME.log("Y Axis: " + window.navigator.getGamepads()[0].axes[1]);
   },
 
 });
